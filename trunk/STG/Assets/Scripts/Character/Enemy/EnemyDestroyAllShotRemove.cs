7d4db13195fdb632401524bdf180efdc6ecc68b7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-09-10 23:00:17 +0900

    Enemy11弾幕パターン調整

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/EnemyDestroyAllShotRemove.cs
@@ -0,0 +1,25 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyDestroyAllShotRemove.cs
+*
+*     * Description : アタッチしたオブジェクトが死亡したら敵弾すべて削除するコンポーネント.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EnemyDestroyAllShotRemove : GameMonoBehaviour
+{
+	void OnDestroy()
+	{
+		var shotList = ReferenceManager.refEnemyShotLayer.GetComponentsInChildren<EnemyShot>();
+		foreach( var shot in shotList )
+		{
+			shot.Explosion();
+		}
+	}
+}
