da82630fc91afec8ee8ce96b7ff9af5d1b8c3a05
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-09-14 01:15:18 +0900

    ドローコールの削減 TextureOffsetAnimationTileを使っている部分をMeshUVOffsetに差し替え。

--- trunk/STG/Assets/Scripts/Utility/MeshUVOffset.cs
+++ trunk/STG/Assets/Scripts/Utility/MeshUVOffset.cs
@@ -35,33 +35,13 @@ public class MeshUVOffset : MonoBehaviourExtension
 	private float intervalX = 0.0f;
 	
 	private float intervalY = 0.0f;
-	
-	private readonly Vector2 UVLeftDown = new Vector2( 0.0f, 0.0f );
-	
-	private readonly Vector2 UVRightDown = new Vector2( 1.0f, 0.0f );
-	
-	private readonly Vector2 UVLeftUp = new Vector2( 0.0f, 1.0f );
-	
-	private readonly Vector2 UVRightUp = new Vector2( 1.0f, 1.0f );
-	
+		
 	// Use this for initialization
 	public override void Start()
 	{
 		mesh = refMesh.mesh;
 		Initialize();
 		UpdateUv();
-		foreach( var u in mesh.uv )
-		{
-			Debug.Log( "uv = " + u );
-		}
-		foreach( var v in mesh.vertices )
-		{
-			Debug.Log( "vertice = " + v );
-		}
-		foreach( var t in mesh.triangles )
-		{
-			Debug.Log( "triangle = " + t );
-		}
 	}
 	
 	// Update is called once per frame
@@ -82,6 +62,11 @@ public class MeshUVOffset : MonoBehaviourExtension
 		timer++;
 	}
 	
+	void OnDestroy()
+	{
+		Destroy( mesh );
+	}
+	
 	private void Initialize()
 	{
 		intervalX = 1.0f / tileX;
@@ -95,13 +80,13 @@ public class MeshUVOffset : MonoBehaviourExtension
 		float right = (intervalX * (offset % tileX)) + intervalX;
 		float bottom = 1.0f - (intervalY * (float)((offset / tileX) + 1));
 		
-		Debug.Log( string.Format( "left = {1}{0}top = {2}{0}right = {3}{0}bottom = {4}{0}",
-			System.Environment.NewLine,
-			left,
-			top,
-			right,
-			bottom
-			));
+//		Debug.Log( string.Format( "left = {1}{0}top = {2}{0}right = {3}{0}bottom = {4}{0}",
+//			System.Environment.NewLine,
+//			left,
+//			top,
+//			right,
+//			bottom
+//			));
 		
 		Vector2[] uvList = new Vector2[4];
 		uvList[0] = new Vector2( left, bottom );
