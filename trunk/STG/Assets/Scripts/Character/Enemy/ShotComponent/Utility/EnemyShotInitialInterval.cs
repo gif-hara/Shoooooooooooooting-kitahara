ce112b4e43c0d2bd84bad9c8ff32da1aceb4abef
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-09-21 14:35:55 +0900

    Stage0後半Bの実装中

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/Utility/EnemyShotInitialInterval.cs
@@ -0,0 +1,26 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyShotInitialInterval.cs
+*
+*     * Description : 敵弾発射間隔の初期化コンポーネント.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EnemyShotInitialInterval : MonoBehaviourExtension
+{
+	/// <summary>
+	/// 要素.
+	/// </summary>
+	public EnemyShotCreateComponent.Element element;
+	
+	public override void Start()
+	{
+		GetComponent<EnemyShotCreator>().interval = element.EvaluteFloorToInt();
+	}
+}
