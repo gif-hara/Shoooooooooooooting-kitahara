511ebbc94a293b6b3715e355d17e7d3d80addd8f
hiroki-kitahara <ash_kitahara@as-kitahara.local>
2013-09-27 19:56:35 +0900

    Enemy13弾幕パターンの実装

---
+++ trunk/STG/Assets/Scripts/Utility/ActiveSetterFromReferenceNull.cs
@@ -0,0 +1,52 @@
+﻿//================================================
+/*!
+    @file   ActiveSetterFromReferenceNull.cs
+
+    @brief  参照オブジェクトがNullになったら指定したオブジェクトのActiveフラグを設定するコンポーネント.
+
+    @author CyberConnect2 Co.,Ltd.  Hiroki_Kitahara.
+*/
+//================================================
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ActiveSetterFromReferenceNull : MonoBehaviourExtension
+{
+	/// <summary>
+	/// Null監視するオブジェクト参照リスト.
+	/// </summary>
+	public Transform[] refNullTarget;
+	
+	/// <summary>
+	/// Activeフラグを設定するオブジェクト参照リスト.
+	/// </summary>
+	public GameObject[] refActiveTarget;
+	
+	/// <summary>
+	/// Activeにするか.
+	/// </summary>
+	public bool isActive;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		bool isProc = true;
+		for( int i=0,imax=refNullTarget.Length; i<imax; i++ )
+		{
+			if( refNullTarget[i] != null )
+			{
+				isProc = false;
+			}
+		}
+		
+		if( !isProc )	return;
+		
+		for( int j=0,jmax=refActiveTarget.Length; j<jmax; j++ )
+		{
+			refActiveTarget[j].SetActive( isActive );
+		}
+		enabled = false;
+	}
+}
