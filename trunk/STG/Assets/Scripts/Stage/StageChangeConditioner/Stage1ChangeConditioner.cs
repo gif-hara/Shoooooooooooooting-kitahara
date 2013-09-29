8f4bcea58f62509feb7f72e8f7c6a033585af025
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-09-29 14:04:07 +0900

    Stage1後半Aの実装

---
+++ trunk/STG/Assets/Scripts/Stage/StageChangeConditioner/Stage1ChangeConditioner.cs
@@ -0,0 +1,21 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : Stage1ChangeConditioner.cs
+*
+*     * Description : .
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Stage1ChangeConditioner : A_StageChangeConditioner
+{
+	public override bool Condition()
+	{
+		return true;
+	}
+}
