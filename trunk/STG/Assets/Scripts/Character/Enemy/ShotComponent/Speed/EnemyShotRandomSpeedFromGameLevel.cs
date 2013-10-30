c64175cec02f0233979f70112d334922946a409d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-10-31 02:21:31 +0900

    Enemy15の実装中

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/Speed/EnemyShotRandomSpeedFromGameLevel.cs
@@ -0,0 +1,27 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyShotRandomSpeedFromGameLevel.cs
+*
+*     * Description : .
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EnemyShotRandomSpeedFromGameLevel : EnemyShotCreateComponent
+{
+	protected override void TuningFromSet ()
+	{
+		float random = element.Evalute();
+		owner.speed = Random.Range( 0, random );
+	}
+	protected override void TuningFromAdd ()
+	{
+		float random = element.Evalute();
+		owner.speed += Random.Range( 0, random );
+	}
+}
