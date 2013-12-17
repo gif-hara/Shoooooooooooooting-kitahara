4914905cf5514280d299db9ecd968dbf998fc8e5
hiroki-kitahara <karlton@live.jp>
2013-12-17 21:18:11 +0900

    ミス時にSEが再生されるよう実装

---
+++ trunk/STG/Assets/Scripts/Character/Player/PlaySEFromMiss.cs
@@ -0,0 +1,25 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : PlaySEFromMiss.cs
+*
+*     * Description : ゲーム管理者クラス.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+
+public class PlaySEFromMiss : MonoBehaviour, I_MissEvent
+{
+	/// <summary>
+	/// ラベル.
+	/// </summary>
+	[SerializeField]
+	private string label;
+
+	public void OnMiss()
+	{
+		ReferenceManager.Instance.refSoundManager.Play( label );
+	}
+}
