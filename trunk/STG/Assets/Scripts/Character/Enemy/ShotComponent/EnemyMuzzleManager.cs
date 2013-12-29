fcb49a62b5cc0d7591b9b509976491d11510a2a3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2013-12-29 15:44:41 +0900

    EnemyMuzzleManager.csの実装

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/EnemyMuzzleManager.cs
@@ -0,0 +1,38 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    :EnemyMuzzleManager.cs
+*
+*     * Description : 敵銃口管理者クラス.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 敵銃口管理者クラス.
+/// </summary>
+public class EnemyMuzzleManager : GameMonoBehaviour
+{
+	/// <summary>
+	/// 初期発射口Id.
+	/// </summary>
+	[SerializeField]
+	private int initMuzzleId;
+
+	/// <summary>
+	/// 銃口リスト.
+	/// </summary>
+	[SerializeField]
+	private List<GameObject> refMuzzleList;
+
+	void Start()
+	{
+		for( int i=0,imax=refMuzzleList.Count; i<imax; i++ )
+		{
+			refMuzzleList[i].SetActive( i == initMuzzleId );
+		}
+	}
+}
