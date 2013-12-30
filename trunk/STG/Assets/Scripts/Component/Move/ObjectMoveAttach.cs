127f11c4557eb71186e05e74deb7ce66c58e9dff
hiroki-kitahara <karlton@live.jp>
2013-12-30 15:45:15 +0900

    EnemyMuzzleManagerの実装
    Enemy18の実装中

---
+++ trunk/STG/Assets/Scripts/Component/Move/ObjectMoveAttach.cs
@@ -0,0 +1,39 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : ObjectMoveAttach.cs
+*
+*     * Description : ObjectMove系コンポーネントをアタッチするコンポーネント.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ObjectMove系コンポーネントをアタッチするコンポーネント.
+/// </summary>
+public class ObjectMoveAttach : GameMonoBehaviour, I_MuzzleEventActinable
+{
+	[SerializeField]
+	private A_ObjectMove prefabObjectMove;
+
+	[SerializeField]
+	private Transform refTrans;
+
+	/// <summary>
+	/// 銃口がアクティブになった際のイベント.
+	/// </summary>
+	public void OnActiveMuzzle()
+	{
+		Attach();
+	}
+	/// <summary>
+	/// アタッチ処理.
+	/// </summary>
+	private void Attach()
+	{
+		var objectMove = InstantiateAsChild( refTrans, prefabObjectMove.gameObject ).GetComponent<A_ObjectMove>();
+		objectMove.refTrans = refTrans;
+	}
+}
