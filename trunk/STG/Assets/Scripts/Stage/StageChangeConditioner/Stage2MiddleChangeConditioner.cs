5bb0d43b4b3880c56f8bea23987332da03406c73
hiroki-kitahara <karlton@live.jp>
2013-12-31 14:10:36 +0900

    Stage2実装中

---
+++ trunk/STG/Assets/Scripts/Stage/StageChangeConditioner/Stage2MiddleChangeConditioner.cs
@@ -0,0 +1,22 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : Stage2MiddleChangeConditioner.cs
+*
+*     * Description : .
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// .
+/// </summary>
+public class Stage2MiddleChangeConditioner : A_StageChangeConditioner
+{
+	public override bool Condition ()
+	{
+		return false;
+	}
+}
