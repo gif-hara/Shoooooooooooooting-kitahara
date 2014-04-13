03e543e745b587862c65b8988249a003d724120a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-04-13 22:59:23 +0900

    Bomb3実装

---
+++ trunk/STG/Assets/Scripts/Utility/ScaleAnimation.cs
@@ -0,0 +1,39 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    :ScaleAnimation.cs
+*
+*     * Description : スケールアニメーションコンポーネント.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class ScaleAnimation : GameMonoBehaviour
+{
+	[SerializeField]
+	private Vector3 from;
+
+	[SerializeField]
+	private Vector3 to;
+
+	[SerializeField]
+	private AnimationCurve curve;
+
+	[SerializeField]
+	private int duration;
+
+	private int currentDuration = 0;
+
+	public override void Update ()
+	{
+		base.Update ();
+		Trans.localScale = Vector3.Lerp( from, to, curve.Evaluate( (float)currentDuration / (float)duration ) );
+		currentDuration++;
+	}
+}
