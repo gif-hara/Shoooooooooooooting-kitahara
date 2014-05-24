472ba77fb047fca41a82938bfb87852553cd1629
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-05-24 19:25:52 +0900

    爆発オブジェクトの実装

--- trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/OnEnemyActionObjectMoveAttach.cs
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/OnEnemyActionObjectMoveAttach.cs
@@ -13,28 +13,8 @@ using System.Collections;
 /// <summary>
 /// ObjectMove系コンポーネントをアタッチするコンポーネント.
 /// </summary>
-public class OnEnemyActionObjectMoveAttach : GameMonoBehaviour, I_MuzzleEventActinable
+public class OnEnemyActionObjectMoveAttach : ObjectMoveAttach, I_MuzzleEventActinable
 {
-	[SerializeField]
-	private A_ObjectMove prefabObjectMove;
-
-	[SerializeField]
-	private Transform refTrans;
-
-	[SerializeField]
-	private GameObject refExtensionEventObject;
-
-	/// <summary>
-	/// 生成したオブジェクトに対して拡張処理を行うメッセージ.
-	/// 引数に<c>GameObject createdObject</c>が入る.
-	/// </summary>
-	private const string ExtensionMessage = "OnExtensionObjectMoveAttach";
-
-	/// <summary>
-	/// 生成したプレハブ保持.
-	/// </summary>
-	private GameObject currentObject = null;
-
 	/// <summary>
 	/// 銃口がアクティブになった際のイベント.
 	/// </summary>
@@ -48,22 +28,5 @@ public class OnEnemyActionObjectMoveAttach : GameMonoBehaviour, I_MuzzleEventAct
 	public void OnEnemyShotCreate()
 	{
 		Attach();
-	}
-	/// <summary>
-	/// アタッチ処理.
-	/// </summary>
-	private void Attach()
-	{
-		currentObject = InstantiateAsChild( refTrans, prefabObjectMove.gameObject );
-		var list = currentObject.GetComponents<A_ObjectMove>();
-		System.Array.ForEach<A_ObjectMove>( list, a =>
-		{
-			a.refTrans = refTrans;
-		});
-
-		if( refExtensionEventObject != null )
-		{
-			refExtensionEventObject.SendMessage( ExtensionMessage, currentObject, SendMessageOptions.DontRequireReceiver );
-		}
 	}
 }
---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/OnStartObjectMoveAttach.cs
@@ -0,0 +1,22 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : OnStartObjectMoveAttach.cs
+*
+*     * Description : ObjectMove系コンポーネントをアタッチするコンポーネント.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ObjectMove系コンポーネントをアタッチするコンポーネント.
+/// </summary>
+public class OnStartObjectMoveAttach : ObjectMoveAttach
+{
+	public override void Start ()
+	{
+		Attach();
+	}
+}
