d9c31a3518e67950bebeb0a4c1fbe18450efbc37
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-07-26 21:21:27 +0900

    Enemy39とEnemy40の実装

---
+++ trunk/STG/Assets/Scripts/Utility/PositionLerpMove.cs
@@ -0,0 +1,60 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : PositionLerpMove.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyProject
+{
+	/// <summary>
+	/// 二点間のTransformから座標を設定するコンポーネント.
+	/// </summary>
+	public class PositionLerpMove : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform refFrom;
+
+		[SerializeField]
+		private Transform refTo;
+
+		[SerializeField]
+		private AnimationCurve curve;
+
+		[SerializeField]
+		private int duration;
+
+		[SerializeField]
+		private int delay;
+
+		private Transform cachedTrans;
+
+		private int timer;
+
+		void Start ()
+		{
+			this.cachedTrans = transform;
+		}
+		
+		void Update ()
+		{
+			if( this.delay > 0 )
+			{
+				this.delay--;
+				return;
+			}
+
+			this.cachedTrans.localPosition = Vector3.Lerp( refFrom.localPosition, refTo.localPosition, curve.Evaluate( (float)this.timer / this.duration ) );
+			this.timer++;
+
+			if( this.timer > this.duration )
+			{
+				this.timer = 0;
+			}
+		}
+	}
+}
---
+++ trunk/STG/Assets/Scripts/Utility/PositionLerpSetter.cs
@@ -0,0 +1,40 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : PositionLerpSetter.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyProject
+{
+	/// <summary>
+	/// 二点間のTransformの正規化した座標に設定するコンポーネント.
+	/// </summary>
+	public class PositionLerpSetter : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform refFrom;
+		
+		[SerializeField]
+		private Transform refTo;
+
+		[SerializeField]
+		private float duration;
+		
+		private Transform cachedTrans;
+		
+		void Start ()
+		{
+			this.cachedTrans = transform;
+		}
+		
+		void Update ()
+		{
+			this.cachedTrans.localPosition = Vector3.Lerp( refFrom.localPosition, refTo.localPosition, this.duration );
+		}
+	}
+}
