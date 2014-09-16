0fbf1a372504e30d0dafe1858ea44573de340797
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-09-17 00:58:44 +0900

    SE修正

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/DeadEvent/PlaySEFromDeadRandom.cs
@@ -0,0 +1,33 @@
+﻿//================================================
+/*!
+    @file   PlaySEFromDeadRandom.cs
+
+    @brief  敵死亡時にランダムでSEを再生するコンポーネント.
+
+    @author CyberConnect2 Co.,Ltd.  Hiroki_Kitahara.
+*/
+//================================================
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 敵死亡時にランダムでSEを再生するコンポーネント.
+/// </summary>
+public class PlaySEFromDeadRandom : GameMonoBehaviour, I_DeadEvent
+{
+	/// <summary>
+	/// ラベル.
+	/// </summary>
+	[SerializeField]
+	private List<string> labels;
+
+	/// <summary>
+	/// 死亡処理.
+	/// </summary>
+	public void OnDead()
+	{
+		var label = labels[Random.Range( 0, labels.Count )];
+		ReferenceManager.Instance.refSoundManager.Play( label );
+	}
+}
