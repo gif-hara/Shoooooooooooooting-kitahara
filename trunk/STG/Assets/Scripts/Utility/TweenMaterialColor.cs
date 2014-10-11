a29845a3cc80d9ca35d2c287c5a77fb0c665c6a7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-10-12 01:43:12 +0900

    Enemy14登場演出の実装

---
+++ trunk/STG/Assets/Scripts/Utility/TweenMaterialColor.cs
@@ -0,0 +1,83 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : TweenMaterialColor.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class TweenMaterialColor : MonoBehaviour
+{
+	[SerializeField]
+	private Renderer refRenderer;
+
+	[SerializeField]
+	private string colorName;
+	
+	[SerializeField]
+	private int delay;
+	
+	[SerializeField]
+	private int duration;
+	
+	[SerializeField]
+	private Color from = Color.white;
+	
+	[SerializeField]
+	private Color to = Color.white;
+	
+	[SerializeField]
+	private GameObject refCompleteEventReceiver;
+	
+	[SerializeField]
+	private string completeFunctionName;
+	
+	private MeshColorManager meshManager;
+	
+	private int currentDuration;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentDuration = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if( delay > 0 )
+		{
+			delay--;
+			return;
+		}
+		if( currentDuration > duration )
+		{
+			enabled = false;
+			if( refCompleteEventReceiver != null )
+			{
+				refCompleteEventReceiver.SendMessage( completeFunctionName );
+			}
+			
+			return;
+		}
+		refRenderer.material.SetColor( colorName, Color.Lerp( from, to, currentDuration / (float)duration ) );
+		currentDuration++;
+	}
+	
+	public void SetDelay( int value )
+	{
+		this.delay = value;
+	}
+	
+	public void Begin()
+	{
+		enabled = true;
+		currentDuration = 0;
+	}
+}
