8bb920f23f44accc758cb23669fe24ea459d202c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-02-07 00:10:58 +0900

    Enemy74の実装

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/Utility/EnemyShotLockFromReferenceNull.cs
@@ -0,0 +1,36 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyShotLockFromReferenceNull.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class EnemyShotLockFromReferenceNull : EnemyShotCreateComponent
+{
+	/// <summary>
+	/// Nullであるか監視するオブジェクト.
+	/// </summary>
+	[SerializeField]
+	private GameObject refTarget;
+
+	/// <summary>
+	/// refTargetがNullなら弾を発射する.
+	/// </summary>
+	[SerializeField]
+	private bool isNullActive;
+
+	protected override void TuningFromSet()
+	{
+		owner.isLock = !((refTarget == null) == isNullActive);
+	}
+	protected override void TuningFromAdd()
+	{
+		TuningFromSet();
+	}
+}
