e6ce44eb51e3bb9092121991d1e5794519acbcc7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-02-17 19:12:16 +0900

    Extraステージ実装中

---
+++ trunk/STG/Assets/Scripts/Component/Stage/StageChangeConditioner/ExtraStageChangeConditioner.cs
@@ -0,0 +1,21 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : ExtraStageChangeConditioner.cs
+*
+*     * Description : Extraステージ切り替え条件処理.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ExtraStageChangeConditioner : A_StageChangeConditioner
+{
+	public override bool Condition()
+	{
+		return ReferenceManager.Instance.RefPlayerStatusManager.MissCount == 0;
+	}
+}
