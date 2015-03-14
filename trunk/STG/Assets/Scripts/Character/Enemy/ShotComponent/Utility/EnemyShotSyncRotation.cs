52ab19730d9621afefda936620d083a2c8ebefc8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-03-14 19:07:11 +0900

    Enemy55の弾幕で不備があったので修正

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/Utility/EnemyShotSyncRotation.cs
@@ -0,0 +1,35 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyShotSyncRotation.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class EnemyShotSyncRotation : EnemyShotCreateComponentSeparate
+{
+	[SerializeField]
+	private Transform refTarget;
+
+	[SerializeField]
+	private Vector3 offset;
+
+	protected override void TuningFromSet ()
+	{
+		if( (owner.TotalShotCount + 1) % separate == 0 )
+		{
+			this.cachedTransform.rotation = refTarget.rotation;
+			this.cachedTransform.rotation *= Quaternion.Euler( offset );
+		}
+	}
+	
+	protected override void TuningFromAdd ()
+	{
+		TuningFromSet();
+	}
+}
