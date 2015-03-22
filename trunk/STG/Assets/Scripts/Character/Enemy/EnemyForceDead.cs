f53730aa12dca52432bed14cca9d0d7563a38acf
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-03-22 20:42:27 +0900

    Enemy25・36調整

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/EnemyForceDead.cs
@@ -0,0 +1,34 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyForceDead.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// 強制的に敵を死亡させるコンポーネント.
+/// </summary>
+public class EnemyForceDead : MonoBehaviour
+{
+	[SerializeField]
+	private EnemyController refEnemy;
+
+	[SerializeField]
+	private int delay;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if( this.delay > 0 )
+		{
+			this.delay--;
+			return;
+		}
+
+		refEnemy.ForceDead();
+		enabled = false;
+	}
+}
