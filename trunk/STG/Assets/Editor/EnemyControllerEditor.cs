6b839a617244b6dfdfc544fd9cba28563f3128e1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-05-31 14:58:04 +0900

    全ての敵のShotコンポーネントを参照するよう修正

--- trunk/STG/Assets/Editor/EnemyControllerEditor.cs
+++ trunk/STG/Assets/Editor/EnemyControllerEditor.cs
@@ -12,21 +12,21 @@ using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
-[CustomEditor( typeof( EnemyController ) )]
-public class EnemyControllerEditor : A_EditorMonoBehaviour<EnemyController>
-{
-	public override void OnInspectorGUI()
-	{
-		base.OnInspectorGUI();
-		OnInspectorSetShotCreatorList();
-	}
-	private void OnInspectorSetShotCreatorList()
-	{
-		Button( "Set ShotCreatorList", () =>
-		{
-			Target.refShotCreatorList.Clear();
-			Target.refShotCreatorList.AddRange( Target.Trans.GetComponentsInChildren<EnemyShotCreator>() );
-		});
+//[CustomEditor( typeof( EnemyController ) )]
+//public class EnemyControllerEditor : A_EditorMonoBehaviour<EnemyController>
+//{
+//	public override void OnInspectorGUI()
+//	{
+//		base.OnInspectorGUI();
+//		OnInspectorSetShotCreatorList();
+//	}
+//	private void OnInspectorSetShotCreatorList()
+//	{
+//		Button( "Set ShotCreatorList", () =>
+//		{
+//			Target.refShotCreatorList.Clear();
+//			Target.refShotCreatorList.AddRange( Target.Trans.GetComponentsInChildren<EnemyShotCreator>() );
+//		});
 //		Button( "Create Damage Event", () =>
 //		{
 //			var obj = new GameObject( "DamageEvent" );
@@ -37,5 +37,5 @@ public class EnemyControllerEditor : A_EditorMonoBehaviour<EnemyController>
 //			var flash = obj.AddComponent<DamageEventTextureFlash>();
 //			flash.refEnemy = Target;
 //		});
-	}
-}
+//	}
+//}
