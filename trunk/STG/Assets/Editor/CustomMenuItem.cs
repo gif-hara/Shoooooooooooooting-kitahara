140320d7ecfb9d36923a9a7856d45d4d10da73de
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-06-01 23:22:15 +0900

    ビルド設定ショートカットキーの作成

---
+++ trunk/STG/Assets/Editor/CustomMenuItem.cs
@@ -0,0 +1,28 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : CustomMenuItem.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class CustomMenuItem : MonoBehaviour
+{
+	[MenuItem("STG/Debug Mode %#1")]
+	static void SetDefineDebugMode()
+	{
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "STG_DEBUG");
+	}
+
+	[MenuItem("STG/Release Mode %#2")]
+	static void SetDefineReleaseMode()
+	{
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "STG_RELEASE");
+	}
+}
