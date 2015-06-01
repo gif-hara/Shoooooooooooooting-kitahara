51f91200a999c3f4476e313fc3cfac321709eea4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-06-01 22:00:53 +0900

    ボスラッシュで裏ルートボス突入条件にノーミスを追加

--- trunk/STG/Assets/Scripts/Component/Stage/StageChangeConditioner/BossRushDifficultyChangeConditioner.cs
+++ trunk/STG/Assets/Scripts/Component/Stage/StageChangeConditioner/BossRushDifficultyChangeConditioner.cs
@@ -16,6 +16,11 @@ public class BossRushDifficultyChangeConditioner : A_StageChangeConditioner
 {
 	public override bool Condition()
 	{
+		if( ReferenceManager.Instance.RefPlayerStatusManager.MissCount >= 1 )
+		{
+			return false;
+		}
+
 		return
 			GameManager.DifficultyType == GameDefine.DifficultyType.Hard ||
 			GameManager.DifficultyType == GameDefine.DifficultyType.Hell;
