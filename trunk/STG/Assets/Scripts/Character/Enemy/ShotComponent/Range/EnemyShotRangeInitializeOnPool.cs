e4d97082c95f42f5ad638025efa87695a3ffb2d0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-06-06 13:59:00 +0900

    EnemyShot22の修正

---
+++ trunk/STG/Assets/Scripts/Character/Enemy/ShotComponent/Range/EnemyShotRangeInitializeOnPool.cs
@@ -0,0 +1,31 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : EnemyShotRangeInitializeOnPool.cs
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// .
+/// </summary>
+public class EnemyShotRangeInitializeOnPool : MonoBehaviour, I_Poolable
+{
+	[SerializeField]
+	private EnemyShotCreator refCreator;
+	
+	[SerializeField]
+	private EnemyShotCreateComponent.Element element;
+	
+	public void OnAwakeByPool( bool used )
+	{
+		refCreator.range = element.Evalute();
+	}
+	
+	public void OnReleaseByPool()
+	{
+		
+	}
+}
